be5a63e2992307b3b74e222d47e4ca336380f4a9
romakolesnikow <romakolesnikow@mail.ru>
2022-09-13 01:33:08 +0300

    first commit

--- js/click-counter/index.js
+++
@@ -1,9 +0,0 @@
-function main () {
-  var panelsElements = document.querySelector('.wrapper');
-  [].slice.apply(panelsElements.children).forEach(function (panelElement) {
-    var panel = new Panel();
-    panel.init(panelElement);
-  });
-}
-
-window.onload = main;
